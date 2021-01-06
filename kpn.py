68a735d80972dd7b6cf14b08ec908f2b4643656e
RadoslawP <radoslaw.pietraszek.r@gamail.com>
2021-01-06 16:20:59 +0100

    gra w kamien, papier i nozyce

--- kpn.py
+++ kpn.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import random
+zwyciezca=''
 liczba = random.randint(0, 2)
 
 if liczba==0:
@@ -13,3 +14,16 @@ else:
 
 wybor_uzytkownika=input('Kamien, papier czy nozyce? ')
 print ("Wybrales "+wybor_uzytkownika+ ", a komputer wybral "+wybor_komputera+"!")
+
+if wybor_komputera==wybor_uzytkownika:
+	zwyciezca='Remis'
+elif wybor_komputera=='papier' and wybor_uzytkownika=='kamien':
+	zwyciezca='Komputer'
+elif wybor_komputera=='kamien' and wybor_uzytkownika=='nozyce':
+	zwyciezca='Komputer'
+elif wybor_komputera=='nozyce' and wybor_uzytkownika=='papier':
+	zwyciezca='Komputer'
+else:
+	zwyciezca='Uzytkownik'
+
+print(zwyciezca, 'wygral!')
